d2e9e566b900f357bb1e1ba2e95cca9011601874
Itay Fintz <itayfi@gmail.com>
2024-05-26 22:29:39 +0300

    Add share button

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -1,8 +1,19 @@
-import { Camera, CircleHelp, Upload } from "lucide-react";
+import { Camera, CircleHelp, Share2, Upload } from "lucide-react";
 import { DrawerContent, DrawerRoot, DrawerTrigger } from "./ui/Drawer";
 import { useMemo, useRef, useState } from "react";
+import { createGame } from "../lib/games";
+import { scaleImage } from "../lib/image-tools";
+import { WIDTH } from "../lib/consts";
+import { useMutation } from "@tanstack/react-query";
 
 export type Settings = { image: File; correctAnswers: string[] };
+
+const splitAnwsers = (answers: string) => {
+  return answers
+    .split("\n")
+    .map((x) => x.trim())
+    .filter((x) => x.length > 0);
+};
 
 export const SettingDialog = ({
   onChange,
@@ -12,16 +23,36 @@ export const SettingDialog = ({
   const [file, setFile] = useState<File>();
   const [open, setOpen] = useState(false);
   const correctAnswersRef = useRef<HTMLTextAreaElement | null>(null);
+  const {
+    mutate: onShare,
+    isPending: isSharing,
+    data: sharedUrl,
+    reset: resetShare,
+  } = useMutation({
+    mutationFn: async () => {
+      if (!file || correctAnswersRef.current?.value?.length === 0)
+        return undefined;
+      const smallImage = await scaleImage(URL.createObjectURL(file), WIDTH);
+      const gameData = {
+        answers: splitAnwsers(correctAnswersRef.current!.value),
+        imageData: smallImage.data,
+      };
+      const gameId = await createGame(gameData);
+      const gameUrl = `${window.location.origin}${
+        import.meta.env.BASE_URL
+      }?${gameId}`;
+      navigator.clipboard.writeText(gameUrl);
+
+      return gameUrl;
+    },
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!file || !correctAnswersRef.current) return;
     onChange({
       image: file,
-      correctAnswers: correctAnswersRef.current.value
-        .split("\n")
-        .map((x) => x.trim())
-        .filter((x) => x.length > 0),
+      correctAnswers: splitAnwsers(correctAnswersRef.current.value),
     });
     setOpen(false);
   };
@@ -29,6 +60,7 @@ export const SettingDialog = ({
   const onOpenChange = (open: boolean) => {
     setOpen(open);
     setFile(undefined);
+    resetShare();
   };
 
   const fileImageUrl = useMemo(() => file && URL.createObjectURL(file), [file]);
@@ -93,6 +125,25 @@ export const SettingDialog = ({
               className="border border-black rounded px-2 py-1 w-full outline-none focus-visible:ring focus-visible:ring-yellow-500 focus-visible:ring-offset-1"
             />
           </label>
+          <div>
+            {sharedUrl ? (
+              <input
+                type="text"
+                readOnly
+                value={sharedUrl}
+                className="border border-black rounded px-2 py-1 w-full outline-none focus-visible:ring focus-visible:ring-yellow-500 focus-visible:ring-offset-1"
+              />
+            ) : (
+              <button
+                type="button"
+                className="bg-yellow-300 py-1.5 px-3 rounded cursor-pointer disabled:animate-pulse disabled:cursor-default"
+                onClick={() => onShare()}
+                disabled={isSharing}
+              >
+                <Share2 className="inline-block" /> שתף משחק
+              </button>
+            )}
+          </div>
           <div className="flex justify-end gap-1.5">
             <DrawerTrigger
               type="reset"
